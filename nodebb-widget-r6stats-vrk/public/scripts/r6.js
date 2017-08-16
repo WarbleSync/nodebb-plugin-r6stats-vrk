268047405a26ed8945757f11fd534e7e8ddb4f0e
WarbleSync <warblesync@gmaile.com>
2017-08-16 18:55:22 -0400

    update

--- nodebb-widget-r6stats-vrk/public/scripts/r6.js
+++
@@ -1,9 +0,0 @@
-function showData(target){
-  var tr = target.nextElementSibling
-  if(tr.style.display === 'none'){
-    tr.style.display = 'block'
-  } else {
-    tr.style.display = 'none'
-  }
-  // console.log(target.nextElementSibling.style.display)
-}
--- nodebb-widget-r6stats-vrk/public/templates/widget.tpl
+++
@@ -1,4 +0,0 @@
-<div class="checkbox">
-  <label><input id="ranked" name="ranked" type="checkbox" >Show Ranked? (default is casual)</label>
-  <label><input id="badge" name="badge" type="checkbox" >Show Badge? (default is bust)</label>
-</div>
